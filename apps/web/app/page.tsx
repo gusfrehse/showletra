d161c0d0030da2f7b6a84cc5a29830793dd729d7
Gustavo S Frehse <gusfrehse@gmail.com>
2025-09-25 17:01:53 -0300

    Fix packages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,9 @@
 import Game from "./components/game";
 import Title from "./components/title"
-import type { Test } from 'utils';
 
 const Home = async () => {
     const res = await fetch("http://" + process.env.NEXT_PUBLIC_SERVER_URL + "/jogo");
     const info = await res.json();
-
-    const t: Test = {
-        test: "ola",
-        opa: "ALOU",
-    };
-
-    console.log(t, typeof t);
 
     return ( 
         <>
